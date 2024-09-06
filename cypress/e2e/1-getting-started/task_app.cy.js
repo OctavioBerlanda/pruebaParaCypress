24371e01f77cf186f3c205710e336e226cce89e0
Octavio <oberlanda@frro.utn.edu.ar>
2024-09-06 10:41:18 -0300

    creando mas pruebas

--- cypress/e2e/1-getting-started/task_app.cy.js
+++ cypress/e2e/1-getting-started/task_app.cy.js
@@ -4,24 +4,31 @@ describe('Task App', () => {
     cy.contains('Tasks');
   })
 
-  it('task form can do gets', () => {
-    cy.get('button').eq(1).click(); // Selecciona el segundo botón (índice empieza en 0)
-    cy.get('input:first').clear().type('Probar aplicación con cypress');
-    cy.get('textarea[name="description"]')
-      .clear() // Borra el contenido actual del textarea
-      .type('Hay que probar aplicación con Cypress');
-    cy.get('button').contains('Save').click();
-    cy.get('button').eq(5).contains('Toggle Task').click();
-    // cy.get('button').eq(3).contains('Delete').click();
-  })
-
   it('task form can create a task', () => {
     cy.get('a').contains('Create task').click();
     cy.get('input[name="title"]')
       .type('Futbol 5 con amigos');
     cy.get('textarea[name="description"]')
       .type('Equipo: pupi lauta renzo alejo mati');
-    cy.get('button').contains('Save').click();
+    cy.get('li').contains('Home').click();
+    cy.get('h2').contains('Futbol 5 con amigos');
+    cy.get('p').contains('Equipo: pupi lauta renzo alejo mati');
   })
+
+  it('task form can edit a task', () => {
+    cy.get('button').eq(1).click(); // Selecciona el segundo botón (índice empieza en 0)
+    cy.get('input:first').clear().type('Hacer proof of concept');
+    cy.get('textarea[name="description"]')
+      .clear() // Borra el contenido actual del textarea
+      .type('Hay que hacer la proof of concept con cypress');
+    cy.get('button').contains('Save').click();
+    cy.get('button').eq(5).contains('Toggle Task').click();
+  })
+
+  it('should mark task as completed', () => {
+    cy.get('button').contains('Toggle Task').click();
+    cy.contains('span', '✅'); // Para verificar una tarea completada
+    cy.contains('span', '❌'); // Para verificar una tarea no completada
+  });
 
 });
